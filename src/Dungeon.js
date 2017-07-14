103a3126cb15f44669bab2c77badb0ac562ee1f2
Jose Arreluce <arreluce.jose@gmail.com>
2017-07-13 21:25:45 -0500

    Naive room overlap detection algorithm implemented, does not display overlapping rooms.

--- src/Dungeon.js
+++ src/Dungeon.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Room extends Component {
   render () {
     return (
-      <div style={this.props.styles}></div>
+      <div className={this.props.class} style={this.props.styles}></div>
     );
   }
 }
@@ -15,19 +15,14 @@ class Dungeon extends Component {
       this.getRoomHeight = this.getRoomHeight.bind(this);
       this.getRoomWidth = this.getRoomWidth.bind(this);
       this.generateRooms = this.generateRooms.bind(this);
+      this.roomOverlap = this.roomOverlap.bind(this);
   }
 
   getRandomPointInCircle (radius) {
-    var t = 2 * Math.PI * Math.random();
-    var u = Math.random() + Math.random();
-    var r = null;
-    if(u > 1) {
-      r = 2 - u;
-    } else {
-      r = u;
-    }
+    var x = Math.floor(Math.random() * radius);
+    var y = Math.floor(Math.random() * radius);
+    return [x,y];
 
-    return [radius * r * Math.cos(t), radius * r * Math.sin(t)];
   }
 
   getRoomHeight () {
@@ -48,31 +43,62 @@ class Dungeon extends Component {
       var x = Math.floor(Math.abs(upperLeftCorner[0]));
       var y = Math.floor(Math.abs(upperLeftCorner[1]));
       var dimensions = {
-        border: '5px solid black',
         marginLeft: x,
         marginTop: y,
         height: height,
         width: width
       };
 
-      rooms.push(<Room styles={dimensions} />);
+      rooms.push(<Room class="room" styles={dimensions} />);
     }
 
     //Should return rooms only. X and Y passed in for dev purposes.
-    return [rooms, x, y];
+    return rooms;
+  }
+
+  roomOverlap(room1, room2) {
+    var room1_x = room1.props.styles['marginLeft'];
+    var room1_y = room1.props.styles['marginTop'];
+    var room1_height = room1.props.styles['height'];
+    var room1_width = room1.props.styles['width'];
+
+    var room2_x = room2.props.styles['marginLeft'];
+    var room2_y = room2.props.styles['marginTop'];
+    var room2_height = room2.props.styles['height'];
+    var room2_width = room2.props.styles['width'];
+
+    if(room1_x > room2_x + room2_width || room2_x > room1_x + room1_width) {
+      return false;
+    }
+
+    if(room1_y + room1_height < room2_y || room2_y + room2_height < room1_y) {
+      return false;
+    }
+
+    return true;
+
   }
 
   render () {
-    var roomInfo = this.generateRooms(1);
+    var rooms = this.generateRooms(10);
+    var finalRooms = [];
+    for (var i = 0; i < rooms.length; i++) {
+      for (var j = i + 1; j < rooms.length; j++) {
+        if(this.roomOverlap(rooms[i], rooms[j])) {
+          break;
+        }
+
+        if(j === rooms.length - 1) {
+          finalRooms.push(rooms[i]);
+        }
+      }
+    }
 
     return (
-      <div>
+      <div id="dungeon">
         <p> This is a WIP </p>
         <p> The Dungeon will go here. </p>
-        <p> Random Point: { roomInfo[1]},
-                          { roomInfo[2]}
-        </p>
-        { roomInfo[0][0] }
+        { finalRooms }
       </div>
     );
   }
