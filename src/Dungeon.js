14df313c6e8cf64412a9b56e3bedabc40a6c5551
Jose Arreluce <arreluce.jose@gmail.com>
2017-07-20 20:35:36 -0500

    Removes overlapping rooms, restricts x and y coordinates to fit on grid.

--- src/Dungeon.js
+++ src/Dungeon.js
@@ -26,7 +26,9 @@ class Dungeon extends Component {
       this.getHeight = this.getHeight.bind(this);
       this.getWidth = this.getWidth.bind(this);
       this.getRandomPoint = this.getRandomPoint.bind(this);
+      this.overlaps = this.overlaps.bind(this);
       this.placeRooms = this.placeRooms.bind(this);
+      this.removeOverlappingRooms = this.removeOverlappingRooms.bind(this);
 
       this.state = {
         gridSize: 10000,
@@ -36,7 +38,7 @@ class Dungeon extends Component {
   }
 
   componentDidMount() {
-    var rooms = this.generateRooms(1);
+    var rooms = this.generateRooms(20);
     var cellTypes = this.placeRooms(rooms);
     var cells = this.generateGrid(10000, cellTypes);
   }
@@ -68,8 +70,8 @@ class Dungeon extends Component {
       var rooms = [];
       for(var i = 0; i < num; i++) {
         var point = this.getRandomPoint(this.state.gridSize);
-        var height = this.getHeight(100 - (point[0] % 100));
-        var width = this.getWidth(100 - (point[0] % 100));
+        var height = this.getHeight(Math.min(100 - (point[0] % 100), 50));
+        var width = this.getWidth(Math.min(100 - (point[0] % 100), 50));
         rooms.push(new Room(height, width, point));
       }
       return rooms;
@@ -84,22 +86,49 @@ class Dungeon extends Component {
   }
 
   getRandomPoint(gridSize) {
-    var x = Math.floor(Math.random() * gridSize);
-    var y = Math.floor(Math.random() * gridSize);
+    var x = Math.floor(Math.random() * (gridSize / 100));
+    var y = Math.floor(Math.random() * (gridSize / 100));
     return [x,y];
+  }
+
+  overlaps(roomA, roomB) {
+      var toRight = roomA.point[0] + roomA.width < roomB.point[0];
+      var toLeft = roomA.point[0] > roomB.point[0] + roomB.width;
+      var above = roomA.point[1] > roomB.point[1] + roomB.height;
+      var below = roomA.point[1] + roomA.height < roomB.height;
+      return !(toRight || toLeft || above || below);
   }
 
   placeRooms(rooms) {
     var cellTypes = this.state.cellTypes;
+    rooms = this.removeOverlappingRooms(rooms);
     for(var i = 0; i < rooms.length; i++) {
       var currRoom = rooms[i];
-      for(var j = currRoom.point[0]; j < currRoom.point[0] + (currRoom.height * 100); j += 100) {
+      var firstPoint = currRoom.point[0] + (currRoom.point[1] * 100);
+      for(var j = firstPoint; j < firstPoint + (currRoom.height * 100); j += 100) {
         for(var k = j; k < j + currRoom.width; k++) {
           cellTypes[k] = "room";
         }
       }
     }
     return cellTypes;
+  }
+
+  removeOverlappingRooms(rooms) {
+      var newRooms = [];
+      var overlapCount = new Array(rooms.length).fill(0);
+      for(var i = 0; i < rooms.length; i++) {
+        if(!overlapCount[i]) {
+          newRooms.push(rooms[i]);
+          for(var j = i + 1; j < rooms.length; j++) {
+            if(this.overlaps(rooms[i], rooms[j])) {
+              overlapCount[j] += 1;
+            }
+          }
+        }
+      }
+
+      return newRooms;
   }
 
   render () {
